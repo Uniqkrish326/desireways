9c0da40bac35c7f8a8a21b54cb9b18f0b852db3c
Uniqkrish326 <krish32627@gmail.com>
2024-10-15 12:33:20 -0700

    Add admin component and update product JSON

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Home from './pages/Home';
 import ModelList from './pages/ModelList';
 import FilteredProductList from './pages/FilteredProductList';
 import ProductDetail from './pages/ProductDetail';
+import Admin from './components/admin'; // Import your admin dashboard
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>  {/* Added basename for routing on GitHub Pages */}
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/products/:category" element={<ModelList />} /> {/* Route for Model List */}
-          <Route path="/products/:category/:model" element={<FilteredProductList />} /> {/* Route for filtered products */}
-          <Route path="/products/:category/:model/:productId" element={<ProductDetail />} /> {/* Route for product details */}
+          <Route path="/admin" element={<Admin />} /> {/* Admin Dashboard Route */}
+          <Route path="/products/:category" element={<ModelList />} />
+          <Route path="/products/:category/:model" element={<FilteredProductList />} />
+          <Route path="/products/:category/:model/:productId" element={<ProductDetail />} />
         </Routes>
       </div>
     </Router>
